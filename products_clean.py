84b9610ca7450b7f8d178cba9dfcedfae6bbfb1f
Peggy LAN <pegger6521@gmail.com>
2022-04-10 23:58:00 +0800

    add read file

---
+++ products_clean.py
@@ -0,0 +1,29 @@
+# 讀取檔案
+products = []
+with open('products_clean.csv', 'r') as f:
+	for line in f:
+		if '項目,價格' in line:
+			continue #繼續下一回
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+# 讓使用者輸入
+while True:
+ 	name = input('請輸入商品名稱: ')
+ 	if name == 'q':
+ 		break
+ 	price = input('請輸入商品價格: ')
+ 	products.append([name, price]) 
+print(products)
+
+# 印出所有購買紀錄
+for p in products:
+	print(p[0],'的價格是',p[1])	
+
+# 寫入檔案
+with open('products_clean.csv', 'w') as f:
+	f.write('項目,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
+	
